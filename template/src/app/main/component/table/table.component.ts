16777d4847f057d03cf2b1d7b2ca60e66aa51448
ivancas84 <ivancas84@gmail.com>
2018-05-20 22:18:20 -0300

    agregar ordenamiento de campos

--- template/src/app/main/component/table/table.component.ts
+++ template/src/app/main/component/table/table.component.ts
@@ -3,22 +3,47 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {ModalConfirmComponent} from '../modal-confirm/modal-confirm.component';
 
 import { DataDefinitionService } from '../../../service/data-definition/data-definition.service';
+import { Display } from "../../class/display";
+import { RouterService } from "../../service/router/router.service";
 
 export abstract class TableComponent implements OnChanges {
   entity: string; //entidad principal del componente
+  @Input() display: Display;
+
 
   @Input() data: any; //datos a formatear
   rows: any = []; //datos a visualizar
+  order: any = {}; //configuracion de ordenamiento
+  url: string; //url de acceso
+
 
   @Input() sync: any = []; //configuracion de sincronizacion
 
-  constructor(protected dd: DataDefinitionService, protected modalService: NgbModal) { }
+  constructor(protected dd: DataDefinitionService, protected modalService: NgbModal, protected router: RouterService) { }
 
   isSync(field: string){ return this.dd.isSync(field, this.sync); }
 
-  order(){
-    console.log("en construccion");
-  }
+  setOrder(){
+
+    var keys = Object.keys(this.display.order);
+
+    if((keys.length) && (arguments[0] == keys[0])){
+      var type: string = (this.display.order[keys[0]].toLowerCase() == "asc") ? "desc" : "asc";
+      this.display.order[keys[0]] = type;
+    } else {
+      var obj = {}
+      for(var i = 0; i < arguments.length; i++) obj[arguments[i]] = "asc";
+      this.display.order = Object.assign(obj, this.display.order);
+    }
+
+    console.log(this.display.order);
+    let sid = encodeURI(JSON.stringify(this.display));
+    this.router.navigateByUrl('/' + this.url + '?sid=' + sid);
+
+  };
+
+
+
 
   ngOnChanges(changes: SimpleChanges) {
     if(changes.data){ //changes.row.isFirstChange()
