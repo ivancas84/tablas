5b2fae8dc15fb7093e7dfc832eb3805072d46edc
Iván <ivancas84@gmail.com>
2018-09-14 14:55:38 -0300

    agregar soporte para strings Y-m-d

--- template/api/main/function/formatDate.php
+++ template/api/main/function/formatDate.php
@@ -1,4 +1,7 @@
 <?
-function formatDate(DateTime $value = null, $format = "d/m/Y"){
+
+//@param $value Instancia de datetime o fecha en formato "Y-m-d"
+function formatDate($value = null, $format = "d/m/Y"){
+  if(gettype($value) === "string") $value = SpanishDateTime::createFromFormat("Y-m-d", $value);
   return ($value) ? $value->format($format) : null;
 }
