c7a40e8f8e4625d49aa4eb1cb4b420da244c6a6e
Iván <ivancas84@gmail.com>
2019-06-27 14:23:05 -0300

    incorporar nueva estructuracion a FieldsetArrayTs

--- gen/generate/angulariogen/component/fieldsetArray/FieldsetArrayTs.php
+++ gen/generate/angulariogen/component/fieldsetArray/FieldsetArrayTs.php
@@ -28,7 +28,7 @@ class ComponentFieldsetArrayTs extends GenerateFileEntity {
   protected function getters(){
     foreach($this->entity->getFieldsByType(["nf","fk"]) as $field){
       if(!$field->isAdmin()) continue;
-      $this->string .= "  {$field->getName('xxYy')}(i) { return this.fieldsetForm[i].get('{$field->getName()}')}
+      $this->string .= "  {$field->getName('xxYy')}(i) { return this.fieldset[i].get('{$field->getName()}')}
 ";
     }
     $this->string .= "
@@ -39,20 +39,21 @@ class ComponentFieldsetArrayTs extends GenerateFileEntity {
   protected function start(){
     $this->string .= "import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { FieldsetArrayComponent } from '../../main/component/fieldset-array/fieldset-array.component';
-import { DataDefinitionService } from '../../service/data-definition/data-definition.service';
-import { ValidatorsService } from '../../main/service/validators/validators.service';
-import { {$this->entity->getName('XxYy')} } from '../../class/entity/{$this->entity->getName('xx-yy')}/{$this->entity->getName('xx-yy')}';
+import { FieldsetArrayComponent } from 'src/app/main/component/fieldset-array/fieldset-array.component';
+import { DataDefinitionService } from 'src/app/service/data-definition/data-definition.service';
+import { ValidatorsService } from 'src/app/main/service/validators/validators.service';
+import { {$this->entity->getName('XxYy')} } from 'src/app/class/entity/{$this->entity->getName('xx-yy')}/{$this->entity->getName('xx-yy')}';
 
 @Component({
   selector: 'app-" . $this->entity->getName("xx-yy") . "-fieldset-array',
   templateUrl: './" . $this->entity->getName("xx-yy") . "-fieldset-array.component.html',
 })
 export class " . $this->entity->getName("XxYy") . "FieldsetArrayComponent extends FieldsetArrayComponent {
+  entityName: string = '" . $this->entity->getName() . "';
+  fieldsetName: string = '" . $this->entity->getName() . "_';
+
   constructor(protected fb: FormBuilder, protected dd: DataDefinitionService, protected validators: ValidatorsService) {
-    super(fb, dd, validators);
-    this.entity = '" . $this->entity->getName() . "';
-    this.fieldset = '" . $this->entity->getName() . "';
+    super(fb, dd, validators);    
   }
 
 ";
