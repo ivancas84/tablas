010b40d9bf04a6d847966e1fe4d8166683b808f4
ivancas84 <ivancas84@gmail.com>
2020-05-22 10:47:14 -0300

    corregir ruta

--- gen/phpdbgen.php
+++ gen/phpdbgen.php
@@ -1,7 +1,7 @@
 <?php
 
 //controlador para generar el proyecto PhpDbGen
-require($_SERVER["DOCUMENT_ROOT"] . "/dea-organizacion/config/config.php"); //configuracion del modulo de administracion
+require($_SERVER["DOCUMENT_ROOT"] . "/config/config.php"); //configuracion del modulo de administracion
 
 require_once("class/model/Entity.php");
 
